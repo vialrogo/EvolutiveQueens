51284441098ef16d115d21003dd9eb0530eaeb54
Victor Alberto Romero <vialrogo@gmail.com>
2013-10-29 17:00:24 -0200

    Hola mundo

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Evolutive Queens:
+ *      Eight queens by the evolutive way
+ *
+ * Victor Alberto Romero
+ * Universidade de São Paulo
+ * Intenigência Artificial
+ * Outubro de 2013
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* archv[])
+{
+    cout<<"Hola mundo"<<endl;
+}
